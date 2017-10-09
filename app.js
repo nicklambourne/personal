668f01839758657aa73f836dd71332701f89afe6
Nicholas Lambourne <ndl93@live.com>
2017-10-09 23:35:36 +1000

    Hello, world

---
+++ app.js
@@ -0,0 +1,20 @@
+// Dependencies
+let express = require("express");
+
+// Initialisation & Settings
+let app = express();
+app.set("views", "views");
+app.set("view engine", "jade");
+
+// Static files
+app.use(express.static("public"));
+
+// Routes
+app.get('/', function (req, res) {
+    res.send("Hello, world!");
+});
+
+// Run
+app.listen(3000, function () {
+    console.log("Server listening on port 3000...");
+});
