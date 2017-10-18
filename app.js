baeeb93d552b914e4df39ef6215f168b4d691431
Nicholas Lambourne <ndl93@live.com>
2017-10-18 14:02:38 +1000

    subdomain support?

--- app.js
+++ app.js
@@ -1,28 +1,34 @@
 // Dependencies
-let express = require("express");
-let sslRedirect = require("heroku-ssl-redirect");
+let express = require('express');
+let sslRedirect = require('heroku-ssl-redirect');
+let subdomain = require('express-subdomain');
+let blogRouter = require('./blog');
+
 
 // Initialisation & Settings
 let app = express();
-app.set("views", "views");
-app.set("view engine", "jade");
+app.set('views', 'views');
+app.set('view engine', 'jade');
 
 // Middleware
 app.use(sslRedirect());
 
-// Static files
-app.use(express.static("public"));
+// My Modules
+app.use(subdomain('blog', blogRouter));
+
+// Static Files
+app.use(express.static('public'));
 app.use(express.static('node_modules/jquery/dist'));
-app.use(express.static("node_modules/bootstrap/dist"));
-app.use(express.static("node_modules/popper.js/dist"));
-app.use(express.static("node_modules/font-awesome"));
+app.use(express.static('node_modules/bootstrap/dist'));
+app.use(express.static('node_modules/popper.js/dist'));
+app.use(express.static('node_modules/font-awesome'));
 
 // Routes
 app.get('/', function (req, res) {
-    res.render("index", {title: "NDL"});
+    res.render('index', {title: 'NDL'});
 });
 
 // Run
 app.listen(process.env.PORT, function () {
-    console.log("Server listening on port " + process.env.PORT + "...");
+    console.log('Server listening on port ' + process.env.PORT + '...');
 });
---
+++ blog.js
@@ -0,0 +1,9 @@
+let express = require('express');
+
+
+let router = express.Router();
+module.exports = router;
+
+router.get('/', function(req, res) {
+    res.send("WIP");
+});
