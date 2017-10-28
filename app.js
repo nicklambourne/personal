204ab164d0209c308e3fe08a8462f1bc7cae47a2
Nicholas Lambourne <ndl93@live.com>
2017-10-29 03:45:25 +1000

    added relative routes

--- app.js
+++ app.js
@@ -2,10 +2,10 @@
 let express = require('express');
 let sslRedirect = require('heroku-ssl-redirect');
 let subdomain = require('express-subdomain');
+let bodyParser = require('body-parser');
 let mongoose = require('mongoose');
 let uuid = require('uuid');
 let blogRouter = require('./blog');
-let models = require('./models/models');
 let aws = require('aws-sdk');
 let s3 = new aws.S3();
 
@@ -13,18 +13,22 @@ let s3 = new aws.S3();
 let app = express();
 app.set('views', 'views');
 app.set('view engine', 'jade');
+
+// Document DB
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGO_URI)
     .catch(function (err) {
         console.log('Database connection failed!' + err);
     }
 );
+
+// Object Storage
 const S3_BUCKET = process.env.S3_BUCKET;
 aws.config.region = 'ap-southeast-2';
 
 // Middleware
 app.use(sslRedirect());
-app.use(subdomain('blog', blogRouter));
+app.use((bodyParser.urlencoded({ extended: true})));
 
 // Static Files
 app.use(express.static('public'));
@@ -34,24 +38,10 @@ app.use(express.static('node_modules/popper.js/dist'));
 app.use(express.static('node_modules/font-awesome'));
 
 // Routes
+let base = (process.env.ENV == 'development') ? 'hexxie.com:'+process.env.PORT.toString() : 'ndl.im';
+app.use(subdomain('blog', blogRouter));
 app.get('/', function (req, res) {
-    res.render('index', {title: 'NDL'});
-});
-
-app.get('/hello', function(req, res) {
-    let id = uuid.v4();
-    let title = "Hi";
-    let content = 'hello';
-    let published = Date.now();
-    let obj = new models.Post({id: id, title: title, published: published, content: content});
-    obj.save(function (err, obj) {
-        if (err) {
-            return console.error(err);
-        } else {
-            console.log('Added object: ' + obj.toString());
-        }
-    });
-    res.redirect('/')
+    res.render('index', {title: 'NDL', base: base, });
 });
 
 // Run
--- blog.js
+++ blog.js
@@ -1,29 +1,64 @@
 let express = require('express');
+let uuid = require('uuid');
 let Post = require('./models/models').Post;
 let _ = require('lodash');
 
+
 let router = express.Router();
 module.exports = router;
+
+let base = (process.env.ENV == 'development') ? 'hexxie.com:'+process.env.PORT.toString() : 'ndl.im';
 
 router.get('/', function(req, res) {
     Post.find({}).sort('-published').exec(function (error, posts) {
         if (error) {
             console.log(error.toString());
         } else {
-            res.render('blog-home', {title: "Blog", posts: posts});
+            res.render('blog-home', {title: "Blog", posts: posts, base: base});
         }
     });
-
 });
 
 router.get('/about', function(req, res) {
-    res.render('about', {title: "About"});
+    res.render('about', {title: "About", base: base});
 });
 
 router.get('/projects', function(req, res) {
-    res.render('projects', {title: "Projects"});
+    res.render('projects', {title: "Projects", base: base});
 });
 
 router.get('/meta', function(req, res) {
-    res.render('meta', {title: "Meta"});
+    res.render('meta', {title: "Meta", base: base});
 });
+
+router.get('/post/new', function (req, res) {
+    res.render('add_post', {title: "New Post", base: base});
+});
+
+router.post('/post/new', function (req, res) {
+    let id = uuid.v4();
+    let title = req.body.title;
+    let content = req.body.content;
+    let published = Date.now();
+    let obj = new Post({id: id, title: title, published: published, content: content});
+    obj.save(function (err, obj) {
+        if (err) {
+            return console.error(err);
+        } else {
+            console.log('Added object: ' + obj.toString());
+        }
+    });
+    res.redirect('/post/'+id);
+});
+
+router.get('/post/:id', function (req, res) {
+    Post.findOne( {'id': req.params.id} ,function (error, post) {
+        if (error) {
+            console.log(error.toString());
+            res.sendStatus(404);
+        } else {
+            res.render('post', {title: 'NDL', post: post, base: base});
+        }
+    });
+});
+
