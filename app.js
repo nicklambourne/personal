0b325134a139d068b191317f86b280ea9715561d
Nicholas Lambourne <ndl93@live.com>
2017-10-29 23:38:36 +1000

    added custom 404 page

--- app.js
+++ app.js
@@ -43,6 +43,10 @@ app.use(subdomain('blog', blogRouter));
 app.get('/', function (req, res) {
     res.render('index', {title: 'NDL', base: base, });
 });
+// Custom 404
+app.use(function(req, res, next){
+    res.status(404).render('404', {title: "Sorry, page not found"});
+});
 
 // Run
 app.listen(process.env.PORT, function () {
