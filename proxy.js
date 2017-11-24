205242c4c65467fcf38226f4883de83f4e31d3df
Nicholas Lambourne <ndl93@live.com>
2017-11-25 02:15:52 +1000

    revert

--- proxy.js
+++ proxy.js
@@ -7,7 +7,8 @@ module.exports = router;
 let base = (process.env.ENV == 'development') ? 'hexxie.com:'+process.env.PORT.toString() : 'ndl.im';
 
 router.get('/sample.mp3', function(req, res) {
-    return res.redirect(req.param('url'));
+    console.log(decodeURI(req.param('url')));
+    return res.redirect(decodeURI(req.param('url')));
 });
 
 
