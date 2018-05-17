8bc70e85e43fc28dac4ff9b169cc5c8f7602f80e
Nicholas Lambourne <ndl93@live.com>
2018-05-16 23:44:29 -0400

    started work on reading refactor

---
+++ api.js
@@ -0,0 +1,37 @@
+let express = require('express');
+let request = require('request');
+let Post = require('./models/models').Post;
+let Book = require('./models/models').Book;
+let _ = require('lodash');
+
+
+let router = express.Router();
+
+
+let base = (process.env.ENV == 'development') ? 'hexxie.com:' + process.env.PORT.toString() : 'ndl.im';
+
+router.get('/admin', function (req, res, next) {
+    // Comment out this line:
+    //res.send('respond with a resource');
+
+    // And insert something like this instead:
+    return res.json([{
+        id: 1,
+        username: "samsepi0l"
+    }, {
+        id: 2,
+        username: "D0loresH4ze"
+    }]);
+});
+
+
+module.exports = router;
+
+//router.get('/', function(req, res) {
+//    res.render('admin', {title: 'Admin', base: 'admin.' + base});
+//});
+
+
+
+
+
