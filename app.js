e4196e27b68380ebf83d7ff8ea427c1589aa9d89
Nicholas Lambourne <ndl93@live.com>
2018-05-22 00:04:11 -0500

    meta typo

--- app.js
+++ app.js
@@ -32,7 +32,6 @@ app.use(sslRedirect());
 app.use((bodyParser.urlencoded({ extended: true})));
 
 // Static Files
-
 app.use(express.static('public'));
 app.use(express.static('node_modules/jquery/dist'));
 app.use(express.static('node_modules/bootstrap/dist'));
