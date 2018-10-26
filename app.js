277e6386f8dcd00044421500cf78e294134d897b
nicklambourne <ndl93@live.com>
2018-10-26 22:49:46 +1000

    ghost blog

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
 // Dependencies
 let express = require('express');
 let sslRedirect = require('heroku-ssl-redirect');
-let subdomain = require('express-subdomain');
 let bodyParser = require('body-parser');
 let mongoose = require('mongoose');
-let path = require('path');
-let blogRouter = require('./blog');
 let apiRouter = require('./api');
-let aws = require('aws-sdk');
-let s3 = new aws.S3();
 
 // Initialisation & Settings
 let app = express();
 app.set('views', 'views');
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
+
+// Middleware
+app.use(sslRedirect());
+app.use((bodyParser.urlencoded({ extended: true})));
 
 // Document DB
 mongoose.Promise = global.Promise;
@@ -22,14 +21,6 @@ mongoose.connect(process.env.MONGO_URI)
         console.log('Database connection failed!' + err);
     }
 );
-
-// Object Storage
-const S3_BUCKET = process.env.S3_BUCKET;
-aws.config.region = 'ap-southeast-2';
-
-// Middleware
-app.use(sslRedirect());
-app.use((bodyParser.urlencoded({ extended: true})));
 
 // Static Files
 app.use(express.static('public'));
@@ -40,20 +31,71 @@ app.use(express.static('node_modules/tinymce'));
 app.use(express.static('node_modules/font-awesome'));
 
 // Routes
-let base = (process.env.ENV == 'development') ? 'hexxie.com:'+process.env.PORT.toString() : 'ndl.im';
+let base = (process.env.ENV === 'development') ? 'hexxie.com:' + process.env.PORT.toString() : 'ndl.im';
 app.use(apiRouter);
-app.use(subdomain('blog', blogRouter));
 
 app.get('/', function (req, res) {
     res.render('index', {title: 'NDL', base: base});
 });
 
+app.get('/about', function(req, res) {
+    res.render('about', {title: 'About', base: base});
+});
+
 app.get('/admin', function (req, res) {
-    res.redirect('https://admin.ndl.im')
+    res.redirect('https://admin.ndl.im');
+});
+
+app.get('/projects', function (req, res) {
+    res.redirect('https://github.com/nicklambourne');
 });
 
 app.get('/cv', function (req, res) {
     res.redirect('https://github.com/nicklambourne/resume/raw/master/cv.pdf');
+});
+
+app.get('/listening', function(req, res) {
+    request.post({
+        url: 'https://api.napster.com/oauth/token',
+        auth: {
+            user: process.env.NAPSTER_KEY,
+            pass: process.env.NAPSTER_SECRET,
+        },
+        form: {
+            username: process.env.NAPSTER_USERNAME,
+            password: process.env.NAPSTER_PASS,
+            grant_type: 'password',
+        }
+    }, function (error, response, body) {
+        if (error) {
+            console.log(error);
+            throw error;
+        }
+        console.log(body);
+        let token = JSON.parse(body).access_token;
+        request.get({
+            url: 'https://api.napster.com/v2.2/me/listens',
+            headers: {
+                Authorization: "Bearer " + token
+            }
+        }, function (error, response, body) {
+            if (error) {
+                console.log(error);
+                throw error;
+            }
+            let napster_data = JSON.parse(body).tracks;
+            console.log(napster_data);
+            res.render('listening', {title: 'NDL',
+                base: 'blog.' + base,
+                proxy: 'proxy.' + base,
+                data: napster_data
+            });
+        })
+    });
+});
+
+app.get('/reading', function(req, res) {
+    res.render('reading', {title: 'Reading', base: base});
 });
 
 // Custom 404
--- models/models.js
+++ models/models.js
@@ -1,18 +1,5 @@
 let mongoose = require('mongoose');
 let Schema = mongoose.Schema;
-
-// Blog Posts
-let postSchema = new Schema({
-    id: String,
-    published: Date,
-    title: String,
-    content: String,
-    draft: Boolean,
-});
-
-let Post = mongoose.model('Post', postSchema);
-exports.Post = Post;
-
 // Reading List Books
 
 let authorSchema = new Schema({
@@ -41,16 +28,3 @@ let bookSchema = new Schema({
 let Book = mongoose.model('Book', bookSchema);
 exports.Book = Book;
 
-// Projects
-let projectSchema = new Schema({
-    id: String,
-    title: String,
-    github_link: String,
-    description: String,
-    post_link: String,
-    img: String,
-    published: Date
-});
-
-let Project = mongoose.model('Project', projectSchema);
-exports.Project = Project;
